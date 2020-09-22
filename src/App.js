738ed3de1928c958038a38da85594198c4742890
Brian Bycynski <high5bri@hotmail.com>
2020-09-22 02:42:02 -0400

    init commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+//
+//
+
+import React from 'react';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className='app'>
+      <h2>Slack</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+//
+//
+
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyChLMOIyiyirxI-oCMK_DljfDzZn4NnVAY',
+  authDomain: 'slack-clone-react-4a7ee.firebaseapp.com',
+  databaseURL: 'https://slack-clone-react-4a7ee.firebaseio.com',
+  projectId: 'slack-clone-react-4a7ee',
+  storageBucket: 'slack-clone-react-4a7ee.appspot.com',
+  messagingSenderId: '667065587263',
+  appId: '1:667065587263:web:44cb1f494ccfecbb63ca8a',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+
+export default db;
